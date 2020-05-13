c85eb4cbc6a9872d9a7e82c57e9e3622103011b0
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-05-13 20:38:22 +0300

    Create dayOfTheWeek.java

---
+++ CodingExercise/ControlFlowStatements/switchStatements/dayOfTheWeek.java
@@ -0,0 +1,74 @@
+public class dayOfTheWeek {
+
+    public static void main(String[] args) {
+
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(7);
+        printDayOfTheWeek(8);
+
+        printDayOfTheWeekIfElse(3);
+
+    }
+
+    public static void printDayOfTheWeek(int day) {
+
+        switch(day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+
+    public static int printDayOfTheWeekIfElse(int day) {
+
+        //using if-else statement
+        //could have been public static void, but I wanted to use the primitive datatype in method signature
+
+        if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else if (day == 7) {
+            System.out.println("Sunday");
+        } else {
+            //everything else that hasn't been tested already, print out below message
+            System.out.println("Invalid day");
+        }
+        return day;
+    }
+}

5176b66c909f0e10b569cf0831d5928e969870f1
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-05-25 01:50:52 +0300

    Create Person.java

---
+++ ClassesConstructorsInheritance/person/Person.java
@@ -0,0 +1,55 @@
+package person;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public boolean isTeen() {
+        if (age > 12 && age < 20) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        } else if (firstName.isEmpty()) {
+            return lastName;
+        } else if (lastName.isEmpty()) {
+            return firstName;
+        }
+
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            age = 0;
+        }
+
+        this.age = age;
+    }
+}

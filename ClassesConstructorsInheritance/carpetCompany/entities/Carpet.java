2d8e00a0ae60887a22ec30c7448f2ad555f55ae2
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-05-29 16:28:18 +0300

    Create Carpet.java

---
+++ ClassesConstructorsInheritance/carpetCompany/entities/Carpet.java
@@ -0,0 +1,33 @@
+package carpetCompany;
+
+public class Carpet {
+
+    /*
+    Write a class with the name Carpet.
+    The class needs one field (instance variable) with name cost of type double.
+     */
+    private double cost;
+
+    /*
+    The class needs to have one constructor with parameter cost of type double
+    and it needs to initialize the field.
+
+    In case the cost parameter is less than 0 it needs to set the cost field value to 0.
+     */
+    public Carpet(double cost) {
+        if (cost < 0) {
+            this.cost = 0;
+        } else {
+            this.cost = cost;
+        }
+    }
+
+    /*
+    Write the following methods (instance methods):
+
+    Method named getCost without any parameters, it needs to return the value of cost field
+     */
+    public double getCost() {
+        return cost;
+    }
+}

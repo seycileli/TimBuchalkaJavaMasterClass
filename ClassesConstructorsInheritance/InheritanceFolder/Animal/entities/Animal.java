71aea5dd32a4b849460b9459ea5a3f4f70ca07fc
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-05-31 18:17:27 +0300

    Create Animal.java

---
+++ ClassesConstructorsInheritance/InheritanceFolder/Animal/entities/Animal.java
@@ -0,0 +1,56 @@
+package animals;
+
+public class Animal {
+
+    //create an instance variable of name of type String and brain, body, size and weight of type int
+
+    private static String name;
+    private static int brain;
+    private static int body;
+    private static int size;
+    private static int weight;
+
+    //create a constructor
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    /*
+    Create 2 methods
+    First method with the name eat
+    Second method with the name move with the parameter speed of type int
+     */
+    public void eat() {
+        System.out.println("Animal.eat() method is called ");
+    }
+
+    public void move(int speed) {
+        System.out.println("Animal.speed() method is called " + speed);
+    }
+
+    //create getters
+    public static String getName() {
+        return name;
+    }
+
+    public static int getBrain() {
+        return brain;
+    }
+
+    public static int getBody() {
+        return body;
+    }
+
+    public static int getSize() {
+        return size;
+    }
+
+    public static int getWeight() {
+        return weight;
+    }
+
+}

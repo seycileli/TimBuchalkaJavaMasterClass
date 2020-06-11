10ae405884ab8d2b0f78f51d7ff89bbc3982474c
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-11 20:47:38 +0300

    Create Animal.java

---
+++ InnerAndAbstractClassesAndInterfaces/AbstractClasses/Animal/entities/Animal.java
@@ -0,0 +1,18 @@
+package entities;
+
+abstract class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void eat();
+    public abstract void breathe();
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void fly();
+}

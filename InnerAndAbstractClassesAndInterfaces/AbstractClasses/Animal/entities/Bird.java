783a5f6a51ea538cfebd3b8c934fb2bcdecad78e
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-11 20:48:24 +0300

    Create Bird.java

---
+++ InnerAndAbstractClassesAndInterfaces/AbstractClasses/Animal/entities/Bird.java
@@ -0,0 +1,21 @@
+package entities;
+
+abstract class Bird extends Animal implements CanFly {
+
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is pecking");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Breathe in, breathe out, repeat");
+
+    }
+
+    public abstract void fly();
+}

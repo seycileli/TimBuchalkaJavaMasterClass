9f5e6fc138a45627662a412754036f44bcf394a4
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-11 20:49:45 +0300

    Create Penguin.java

---
+++ InnerAndAbstractClassesAndInterfaces/AbstractClasses/Animal/entities/Penguin.java
@@ -0,0 +1,14 @@
+package entities;
+
+abstract class Penguin extends Animal {
+
+    public Penguin(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm not very good at that, can I go for a swim instead?");
+    }
+
+}

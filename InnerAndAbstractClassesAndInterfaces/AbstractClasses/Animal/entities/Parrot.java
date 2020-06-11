bf336c0cec24d1311547c209dc51bd68a6a36fb1
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-11 20:49:24 +0300

    Create Parrot.java

---
+++ InnerAndAbstractClassesAndInterfaces/AbstractClasses/Animal/entities/Parrot.java
@@ -0,0 +1,13 @@
+package entities;
+
+abstract class Parrot extends Bird {
+    public Parrot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flitting from branch to branch");
+    }
+
+}

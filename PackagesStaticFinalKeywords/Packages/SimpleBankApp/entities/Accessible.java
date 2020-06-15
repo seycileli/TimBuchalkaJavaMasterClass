7c9bed91ed4a519a43c31c66147d320c353ec829
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-15 22:29:02 +0300

    Create Accessible.java

---
+++ PackagesStaticFinalKeywords/Packages/SimpleBankApp/entities/Accessible.java
@@ -0,0 +1,14 @@
+package entities;
+
+ interface Accessible {
+    /*
+     * Whole point of declaring Interfaces are methods
+     * that have to be implemented*/
+    int SOME_CONSTANT = 100;
+
+    void methodA();
+
+    void methodB();
+
+    boolean methodC();
+}

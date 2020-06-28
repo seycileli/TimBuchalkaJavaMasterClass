95bc445bdcbc5a7212fe39346906e047ea883266
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-28 15:39:56 +0300

    Create StockList.java

---
+++ JavaCollections/Map/MiniCart/entities/StockList.java
@@ -0,0 +1,112 @@
+package entities;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StockList {
+    //key, value
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new LinkedHashMap<>();
+        //this will print our List in alphabetical order vs just using HashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            //check if we have quantities of this item
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+
+            //if there are stocks of this item, adjust the quantity
+            if (inStock != item) {
+                item.adjustStock(inStock.availableQuantity());
+            }
+
+            //adding quantity
+            list.put(item.getName(), item);
+            return item.availableQuantity();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+/*
+        StockItem inStock = list.getOrDefault(item, null);
+
+        if ((inStock != null) && (inStock.availableQuantity() >= quantity) && (quantity >0)) {
+            inStock.adjustStock(-quantity);
+            return quantity; //amount of items taken out of stock
+        }
+        return 0; //nothing was taken out of stock
+
+        substituting the code above for the one below
+*/
+        StockItem inStock = list.get(item);
+
+        if ((inStock != null) && (quantity > 0)) {
+            return inStock.finalizeStock(quantity);
+        }
+        return 0;
+    }
+
+    public int reserveStock(String item, int quantity) {
+        StockItem inStock = list.get(item);
+
+        /*
+        * If the Item is in stock,
+        * and the customer is asking for more than 1 item, then return
+        *
+        * because the customer is interested in making a purchase
+        *
+        * otherwise, return 0,
+        * because the customer is not making a purchase */
+        if ((inStock != null) && (quantity > 0)) {
+            return inStock.reservedStock(quantity);
+        }
+        return 0;
+    }
+
+    public int unreservedStock(String item, int quantity) {
+        StockItem inStock = list.get(item);
+
+        if ((inStock != null) && (quantity > 0)) {
+            return inStock.unreservedStock(quantity);
+        }
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, Double> PriceList() {
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            prices.put(item.getKey(), item.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
+
+    public Map<String, StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        //will return all items
+        String s = "\n Stock List: \n";
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.availableQuantity();
+
+            s = s + stockItem + ". There are " + stockItem.availableQuantity() + " in stock. Value of items: ";
+            s = s + String.format("%.2f", itemValue)  + "\n";
+            totalCost += itemValue;
+        }
+        return s + " Total stock value: " + String.format("%.2f", totalCost);
+        //%.2f means 2 decimal points// or within 2 decimal places
+    }
+}
